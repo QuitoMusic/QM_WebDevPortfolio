9aed6a9cf8b6c9242a43f3c9ad566e8acbdd8583
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-09-10 16:33:13 -0400

    Changed imprt to react-dom/client per browser advise

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 function Navigation() {
   return (
     <header>
-          
+      <div className="headerName">
+        <div className="header-content">
+          <h1 className= 'text-3xl font-bold'>Francisco González</h1>
+        </div>
+      </div>
     </header>
   );
 }
