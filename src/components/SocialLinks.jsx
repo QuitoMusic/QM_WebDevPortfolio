f1b90c927b9685685e1aa1610c68e77c0fb2ff58
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-11-19 12:04:25 -0500

    fixed routing for compatible resume on SocialLinks

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: '/Francisco-Gonzalez-Full-Stack-Developer.pdf',
+            href: '/FranciscoGonzalezFullStackDeveloper.pdf',
             style: 'rounded-tr-md',
             download: true,
         },
